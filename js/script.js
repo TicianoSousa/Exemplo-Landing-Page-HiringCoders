ca8fe15b5521722b6f6128e808c0037bd57c7394
Ticiano Sousa <ticianoentregas@gmail.com>
2021-07-16 21:27:35 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let data = {
+        nome,
+        email,
+    }
+    let convertData = JSON.stringify(data);
+    localStorage.setItem('lead', convertData);
+
+    let tankyou = document.getElementById('tankyou');
+
+    let carregando = `<p> Um momento ...</p>`;
+
+    let parabens = `<p>Parabéns !!! agora você vai economizar,<br>
+                        e arrasar na cozinha. </p>`;
+    tankyou.innerHTML = carregando; 
+
+    setTimeout(() => {
+        tankyou.innerHTML = parabens
+    },2000);
+
+})
